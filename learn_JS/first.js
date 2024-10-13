20bd1a386d3f21871f8cf95b75358979e64b6e99
200BingSu <lisb21510530@gmail.com>
2024-10-13 14:55:06 +0900

    New mini

---
+++ learn_JS/first.js
@@ -0,0 +1,13 @@
+for (let i = 1; i <= 9; i++) {
+  console.log(`2*` + i);
+}
+let 박스 = 30;
+박스 > 20;
+
+const 야스오 = {
+  Q: () => console.log('찌르기'),
+  W: () => console.log('장막'),
+  HP: 500,
+  MovementSpeed: 350,
+};
+야스오.Q;
