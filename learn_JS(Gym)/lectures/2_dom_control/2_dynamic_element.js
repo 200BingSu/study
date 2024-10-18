19e9d4eb1e1d841f6cbaf37e9a4d555f683c8c6b
200BingSu <lisb21510530@gmail.com>
2024-10-18 18:05:28 +0900

    make To do List

--- learn_JS(Gym)/lectures/2_dom_control/2_dynamic_element.js
+++ learn_JS(Gym)/lectures/2_dom_control/2_dynamic_element.js
@@ -9,11 +9,11 @@ addBtn.addEventListener('click', function () {
   let ul = document.querySelector('ul'); //ul 태그를 ul로 부르겠다.
   li.textContent = inputBox.value; //생성되는 li태그의 텍스트 내용은 inputBox(input의 요소) 내 value 값을 넣는다.
   let button = document.createElement('button');
-  button.textContent = 'X';
-  button.setAttribute('calss', 'remover-btn');
-  button.addEventListener('click', function (event) {
-    event.target.parentNode.remove();
-  });
+    button.textContent = 'X';
+    button.setAttribute('calss', 'remover-btn');
+    button.addEventListener('click', function (event) {
+      event.target.parentNode.remove();
+    });
   li.appendChild(button); //생성된 li는 ul의 자식으로 들어간다
   ul.appendChild(li);
   inputBox.value = '';
