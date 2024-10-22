fe86c4813c127768d7c5ebda96c5c139cd407c6f
200BingSu <lisb21510530@gmail.com>
2024-10-22 20:36:26 +0900

    24.10.22

---
+++ learn_JS(Gym)/lectures/3_javascript/while.js
@@ -0,0 +1,13 @@
+'use-strict'
+
+let count=0;
+while (count<5) {
+  console.log(count + '번째 출력');
+  count++;
+};
+
+let number=0;
+do  {
+  console.log(count + '번째 출력');
+  count++;
+} while (number<5);
