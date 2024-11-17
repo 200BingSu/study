f060d0dbc6c35237526922fe1672195b9456bfb9
200Bingsu <200bingsu@gmail.com>
2024-11-17 19:18:29 +0900

    [24.11.16] li 선택할 때 나머지 li 변경시키기

--- 24.11.16/test.js
+++ 24.11.16/test.js
@@ -1,9 +1,37 @@
-const menuList = document.querySelectorAll(`.menu`);
+// 메뉴목록(li)
+const menuList = document.querySelectorAll('.menu');
 console.log(menuList);
-menuList.forEach(function (menu, index) {
-  const selectedMenu = menu.addEventListener('click', function () {
-    console.log('클릭 메뉴: ', index);
+
+let selectedMenuIndex;
+
+function select(index) {
+  menuList.forEach(function (menu, i) {
+    if (i === index) {
+      menu.classList.add('menu-active');
+      menu.classList.remove('menu-non-active');
+    } else {
+      menu.classList.add('menu-non-active');
+      menu.classList.remove('menu-active');
+    }
   });
-  if (menu === selectedMenu) {
-  }
+}
+
+function resetMenu() {
+  // 모든 메뉴에서 클래스 제거
+  menuList.forEach((menu) => {
+    menu.classList.remove('menu-active');
+    menu.classList.remove('menu-non-active');
+  });
+}
+
+// 내가 선택한 메뉴의 selectedMenuIndex를 select()함수의 기준으로 사용
+menuList.forEach(function (menu, index) {
+  menu.addEventListener('mouseenter', function () {
+    selectedMenuIndex = index;
+    console.log('고른 메뉴', selectedMenuIndex);
+    select(selectedMenuIndex);
+  });
+  menu.addEventListener('mouseleave', function () {
+    resetMenu();
+  });
 });
