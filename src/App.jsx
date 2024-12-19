217045be62cb98c959e58761a1af7d0284bfe953
200Bingsu <200bingsu@gmail.com>
2024-12-19 23:27:25 +0900

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,17 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Layout from "./components/Layout";
 import HomePage from "./pages/HomePage";
-import OrderPage from "./pages/Order/OrderPage";
-import LoginPage from "./pages/LoginPage";
-import Header from "./components/Header";
-import MenuList from "./pages/Order/MenuList";
-import MenuDetail from "./pages/Order/MenuDetail";
-import Bucket from "./pages/Order/bucket";
-import Payment from "./pages/Order/payment";
-import CompleteOrder from "./pages/Order/CompleteOrder";
-import { LoginProvider } from "./contexts/LoginContext";
 
-const App = ({ children }) => {
+const App = () => {
   return (
     <BrowserRouter>
-      <LoginProvider>
-        <Header></Header>
+      <Layout>
         <Routes>
-          {/* 홈 */}
           <Route path="/" element={<HomePage />} />
-          {/* 로그인 */}
-          <Route path="/login" element={<LoginPage />} />
-          {/* 주문 */}
-          <Route path="/order">
-            <Route index element={<OrderPage />} />
-            <Route path="menu-list" element={<MenuList />} />
-            <Route path="menu-detail" element={<MenuDetail />} />
-            <Route path="bucket" element={<Bucket />} />
-            <Route path="payment" element={<Payment />} />
-            <Route path="complete-order" element={<CompleteOrder />} />
-          </Route>
         </Routes>
-      </LoginProvider>
+      </Layout>
     </BrowserRouter>
   );
 };
+
 export default App;
