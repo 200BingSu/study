042fc164785dc82e90ee787c842b08d87b025b51
200BingSu <200bingsu@gmail.com>
2025-07-03 21:34:38 +0900

    postMessage

--- src/components/postMessage/ChildPage.jsx
+++ src/components/postMessage/ChildPage.jsx
@@ -1,7 +1,9 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 
 export default function ChildPage() {
   const [signature, setSignature] = useState("");
+  const [form, setForm] = useState({ name: "" });
+  const [mode, setMode] = useState("default");
 
   const sendSignature = () => {
     if (window.opener) {
@@ -16,14 +18,47 @@ export default function ChildPage() {
     }
   };
 
+  const renderComponent = () => {
+    switch (mode) {
+      case "default":
+        return <>default mode</>;
+      case "form":
+        return (
+          <>
+            <h1>자식 창 (서명 입력)</h1>
+            <input
+              type="text"
+              value={signature}
+              onChange={e => setSignature(e.target.value)}
+              placeholder="서명 입력"
+            />
+            <button
+              onClick={() => {
+                sendSignature();
+                setMode("default");
+              }}
+            >
+              서명 전송
+            </button>
+          </>
+        );
+      default:
+        return <div>알 수 없는 모드입니다.</div>;
+    }
+  };
+
   useEffect(() => {
     const handleMessage = event => {
       if (event.origin !== window.location.origin) return;
 
       if (event.data.type === "init") {
         console.log("부모창 초기 메시지:", event.data.message);
-      } else if (event.data.type === "ping") {
-        console.log("부모에서 보낸 ping:", event.data.message);
+      } else if (event.data.type === "mode") {
+        console.log("부모에서 보낸 mode:", event.data.payload);
+        setMode(event.data.payload);
+      } else if (event.data.type === "form") {
+        // console.log("부모에서 보낸 form:", event.data.payload);
+        setForm(event.data.payload);
       }
     };
 
@@ -31,16 +66,5 @@ export default function ChildPage() {
     return () => window.removeEventListener("message", handleMessage);
   }, []);
 
-  return (
-    <div>
-      <h1>자식 창 (서명 입력)</h1>
-      <input
-        type="text"
-        value={signature}
-        onChange={e => setSignature(e.target.value)}
-        placeholder="서명 입력"
-      />
-      <button onClick={sendSignature}>서명 전송</button>
-    </div>
-  );
+  return <div>{renderComponent()}</div>;
 }
--- src/components/postMessage/ParentPage.jsx
+++ src/components/postMessage/ParentPage.jsx
@@ -1,6 +1,8 @@
-import React, { useEffect, useRef, useState } from "react";
+import Button from "@mui/material/Button";
+import { useEffect, useRef, useState } from "react";
 
 export default function ParentPage() {
+  const [form, setForm] = useState({ name: "" });
   const [signature, setSignature] = useState(null);
   const childWindowRef = useRef(null);
 
@@ -21,13 +23,27 @@ export default function ParentPage() {
     }, 500); // 0.5초 후 전송
   };
 
-  const sendPingToChild = () => {
+  const sendFormToChild = () => {
     if (childWindowRef.current) {
       childWindowRef.current.postMessage(
-        { type: "ping", message: "부모에서 보낸 ping!" },
+        { type: "form", payload: form },
         window.location.origin,
       );
     }
+  };
+
+  const sendChangeModeToChild = () => {
+    if (childWindowRef.current) {
+      childWindowRef.current.postMessage(
+        { type: "mode", payload: "form" },
+        window.location.origin,
+      );
+    }
+  };
+
+  const handleClickSendBtn = () => {
+    sendFormToChild();
+    sendChangeModeToChild();
   };
 
   useEffect(() => {
@@ -47,8 +63,23 @@ export default function ParentPage() {
   return (
     <div>
       <h1>부모 창</h1>
-      <button onClick={openSignatureWindow}>서명 받기</button>
-      <button onClick={sendPingToChild}>자식창에 ping 보내기</button>
+      <input
+        type="text"
+        name="name"
+        id="name"
+        value={form.name}
+        onChange={e => setForm({ ...form, name: e.target.value })}
+        placeholder="이름을 입력하세요"
+      />
+      <Button type="button" onClick={handleClickSendBtn}>
+        폼 보내기
+      </Button>
+      <Button type="button" onClick={openSignatureWindow}>
+        창열기
+      </Button>
+      <Button type="button" onClick={sendChangeModeToChild}>
+        자식창에 ping 보내기
+      </Button>
       {signature && <p>받은 서명: {signature}</p>}
     </div>
   );
